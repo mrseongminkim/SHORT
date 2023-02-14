bb6e4e9690601edc2962c934af9a8c2cb55b3e12
Seongmin Kim <mr.seongminkim@gmail.com>
2023-02-14 21:27:55 +0900

    Update main.py
    implemented repeated state weight method

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ from FAdo.conversions import *
 from queue import PriorityQueue
 
 
-def state_weight(gfa: GFA):
+def state_weight(gfa: GFA) -> str:
+    gfa = gfa.dup()
     pq = PriorityQueue()
     for i in range(1, len(gfa.States) - 1):
         pq.put((gfa.weight(i), i))
-
-    lst = list()
     while not pq.empty():
-        lst.append(pq.get()[1])
+        gfa.eliminate(pq.get()[1])
+    return gfa.delta[gfa.Initial][list(gfa.Final)[0]]
 
-    gfa.eliminateAll(lst)
+def repeated_state_weight(gfa: GFA) -> str:
+    gfa = gfa.dup()
+    for i in range(len(gfa.States) - 2):
+        min_val = gfa.weight(1)
+        min_idx = 1
+        for j in range(2, len(gfa.States) - 1):
+            curr_val = gfa.weight(j)
+            if min_val > curr_val:
+                min_val = curr_val
+                min_idx = j
+        gfa.eliminateState(min_idx)
     return gfa.delta[gfa.Initial][list(gfa.Final)[0]]
 
 def get_fig10():
@@ -41,4 +51,6 @@ def get_fig10():
 
 fig10 = get_fig10()
 state_weight_result = state_weight(fig10)
+repeated_state_weight_result = repeated_state_weight(fig10)
 print(state_weight_result)
+print(repeated_state_weight_result)
