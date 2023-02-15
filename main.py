d8cb84424521e85d6acb5de401ebd0033a760556
Seongmin Kim <mr.seongminkim@gmail.com>
2023-02-15 20:55:29 +0900

    Update main.py
    basic sketchy of decomposition algorithm, need to think how to build subautomatas

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ from FAdo.conversions import *
 from heuristics import *
 from sample import *
 
-def decomposition(nfa: NFA) -> RegExp:
+def decomposition(nfa: NFA, state_weight = False, repeated = False) -> RegExp:
     result = RegExp(nfa.Sigma)
-    subautomatas = vertical_decomposition(nfa)
+    subautomatas = vertical_decomposition(nfa, state_weight, repeated)
     for subautomata in subautomatas:
-        result = CConcat(result, horizontal_decomposition(subautomata))
+        result = CConcat(result, horizontal_decomposition(subautomata, state_weight, repeated))
     return result
 
-def vertical_decomposition(nfa: NFA) -> list:
-    subautomatas = list()
+def vertical_decomposition(nfa: NFA, state_weight, repeated) -> list:
+    subautomatas = []
     bridge_state = list(cutPoints(nfa))
     if not bridge_state:
         return [nfa]
     initial_state = list(nfa.Initial)[0]
     for i in range(len(bridge_state) + 1):
         final_state = bridge_state[i] if i < len(bridge_state) else list(nfa.Final)[0]
-        subautomatas.append(get_subautomata(nfa, initial_state, final_state))
+        subautomatas.append(make_vertical_subautomata(nfa, initial_state, final_state))
         initial_state = final_state
     return subautomatas
 
-def get_subautomata(nfa: NFA, initial_state, final_state) -> NFA:
+def make_vertical_subautomata(nfa: NFA, initial_state, final_state) -> NFA:
     new = NFA()
     new.setSigma(nfa.Sigma)
     new.States = nfa.States[initial_state : final_state + 1]
@@ -38,30 +38,62 @@ def get_subautomata(nfa: NFA, initial_state, final_state) -> NFA:
     del new.delta[final_state]
     return new
 
-def get_family_group(nfa: NFA) -> dict:
-    count = 0
-    possible_group = []
-    ancester = {}
-    stack = []
-    visited = []
-    for forefather in nfa.delta[list(nfa.Initial)]:
-        stack.append(forefather)
-        possible_group.append(count)
-        ancester[forefather] = count
-        count + 1
-    while not stack.empty():
-        curr = stack.pop()
-        for child in nfa.delta[curr]:
-            if child in ancester and ancester[child] != ancester[child]:
-                pass
+def horizontal_decomposition(nfa: NFA, state_weight, repeated) -> RegExp:
+    result = RegExp(nfa.Sigma)
+    group = identify_group(nfa)
+    subautomatas = make_horizontal_subautomata(nfa, group)
+    for subautomata in subautomatas:
+        if len(cutPoints(subautomata)):
+            result = CDisj(result, decomposition(subautomata, state_weight, repeated))
+        elif (state_weight):
+            result = CDisj(result, repeated_state_weight(subautomata)) if repeated else CDisj(result, state_weight(subautomata))
+        else:
+            result = CDisj(result, random_elimination(subautomata))
+    return result
 
-def horizontal_decomposition(nfa: NFA, state_weight = False, repeated = False) -> RegExp:
-    family_group = get_family_group(nfa)
-    for family in family_group:
+def make_horizontal_subautomata(nfa: NFA, group) -> list:
+    subautomatas = []
+    for sub_group in group:
         pass
 
-    if len(cutPoints(nfa)):
-        return decomposition(nfa)
-    if state_weight:
-        return repeated_state_weight(nfa) if repeated else state_weight(nfa)
-    #return random_elimination(nfa)
+def identify_group(nfa: NFA) -> list:
+    gfa = FA2GFA(nfa)
+    index = 0
+    ancestor = {}
+    visited = []
+    group_index = [[i] for i in range(len(gfa.delta[gfa.Initial]))]
+    for s in gfa.delta[gfa.Initial]:
+        ancestor[s] = index
+        index += 1
+        identify_group_visit(gfa, s, ancestor, visited, group_index)
+    group_index = [i for i in group_index if i]
+    group = [[x for x in ancestor if ancestor[x] in group_index[i]] + [gfa.Initial, list(gfa.Final)[0]] for i in range(len(group_index))]
+    return group
+
+def identify_group_visit(gfa: GFA, s, ancestor, visited, group_index):
+    if s not in visited:
+        visited.append(s)
+        if s in gfa.delta:
+            for dest in gfa.delta[s]:
+                if dest in ancestor:
+                    if ancestor[s] != ancestor[dest]:
+                        min_index = min(ancestor[s], ancestor[dest])
+                        max_index = max(ancestor[s], ancestor[dest])
+                        group_index[min_index].append(max_index)
+                        group_index[max_index].pop()
+                else:
+                    ancestor[dest] = ancestor[s]
+                identify_group_visit(gfa, s, ancestor, visited, group_index)
+
+def test_identify_group():
+    nfa = nfa_for_identify_group_test()
+    print(identify_group(nfa))
+
+
+def test_decomposition(state_weight = False, repeated = False):
+    nfa = get_fig8()
+    print(nfa.States)
+    result = decomposition(nfa, state_weight, repeated)
+    print(result)
+
+test_decomposition()
