3ad2673b29f06a23161a03f3f238b44de086c24e
Seongmin Kim <mr.seongminkim@gmail.com>
2023-02-19 16:34:44 +0900

    Update main.py
    finally getting some results

--- main.py
+++ main.py
@@ -4,18 +4,32 @@ from utils.data_loader import *
 from utils.heuristics import *
 
 '''
+gfa = readFromFile('data.txt')
+#gfa.display()
+gfa = convert_nfa_to_gfa(gfa)
+gfa.reorder({(gfa.States).index(x) : int(x) for x in gfa.States})
+
+#result = decompose(gfa, False, False)
+#print(result)
+result = eliminate_by_repeated_state_weight_heuristic(gfa)
+print(result)
+exit()
+#'''
+'''
 exp[0][n][k][d][0]: TIME = 0
 exp[0][n][k][d][1]: LENGTH = 0
 / 20000
 '''
+#'''
 data = load_data()
-'''
-exp = [[[[0, 0] for d in range(2)] for k in range(3)] for n in range(8)]
+
+exp = [[[[[0, 0] for d in range(2)] for k in range(3)] for n in range(8)] for c in range(6)]
 
 for n in range(8):
     for k in range(3):
         for d in range(2):
-            for i in range(20000):
+            for i in range(100):
+                print('n' + str(n + 3) + 'k' + ('2' if not d else ('5' if d == 1 else '10')) + ('s' if not d else 'd') + '\'s ' + str(i + 1) + ' NFA')
                 #eliminate_randomly
                 gfa = data[n][k][d][i].dup()
                 start_time = time.time()
@@ -78,4 +92,4 @@ for n in range(8):
 
 with open('experimental_result.pkl', 'wb') as fp:
     dump(exp, fp)
-'''
+#'''
