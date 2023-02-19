6eaa908e4ca5214fed313aab651ef3267a80142f
Seongmin Kim <mr.seongminkim@gmail.com>
2023-02-19 16:34:33 +0900

    Update fadomata.py
    fixed some bugs...or made new ones

--- utils/fadomata.py
+++ utils/fadomata.py
@@ -11,47 +11,12 @@ def get_weight(gfa: GFA, state: int) -> int:
         self_loop = 1
         weight += gfa.delta[state][state].treeLength() * (len(gfa.predecessors[state]) - self_loop) * (len(gfa.delta[state]) - self_loop)
     for i in gfa.predecessors[state]:
-        if i != state:
+        if i != state and i in gfa.delta:
             weight += gfa.delta[i][state].treeLength() * (len(gfa.delta[state]) - self_loop)
     for i in gfa.delta[state]:
         if i != state:
             weight += gfa.delta[state][i].treeLength() * (len(gfa.predecessors[state]) - self_loop)
     return weight
-
-def get_bridge_states(gfa: GFA) -> set:
-    '''Counterpart of cutPoints function'''
-    new = gfa.dup()
-    new_edges = []
-    for a in new.delta:
-        for b in new.delta[a]:
-            new_edges.append((a, b))
-    for i in new_edges:
-        if i[1] not in new.delta:
-            new.delta[i[1]] = {}
-        else:
-            new.delta[i[1]][i[0]] = 'x'
-    for i in new_edges:
-        if i[0] not in new.delta[i[1]]:
-            new.delta[i[1]][i[0]] = 'x'
-    new.c = 1
-    new.num = {}
-    new.visited = []
-    new.parent = {}
-    new.low = {}
-    new.cuts = set([])
-    new.assignNum(new.Initial)
-    new.assignLow(new.Initial)
-    new.cuts.remove(new.Initial)
-    cutpoints = copy.copy(new.cuts) - new.Final
-    new = gfa.dup()
-    for i in new.delta:
-        if i in new.delta[i]:
-            del new.delta[i][i]
-    cycles = new.evalNumberOfStateCycles()
-    for i in cycles:
-        if cycles[i] != 0 and i in cutpoints:
-            cutpoints.remove(i)
-    return cutpoints
 
 def convert_nfa_to_gfa(nfa: NFA) -> GFA:
     '''Counterpart of FA2GFA function'''
@@ -73,14 +38,14 @@ def convert_nfa_to_gfa(nfa: NFA) -> GFA:
     return gfa
 
 def add_transition(gfa: GFA, sti1: int, sym: RegExp, sti2: int):
-        '''Counterpart of GFA.addTransition'''
-        if sti1 not in gfa.delta:
-            gfa.delta[sti1] = {}
-        if sti2 not in gfa.delta[sti1]:
-            gfa.delta[sti1][sti2] = sym
-        else:
-            gfa.delta[sti1][sti2] = reex.CDisj(gfa.delta[sti1][sti2], sym, copy.copy(gfa.Sigma))
-        try:
-            gfa.predecessors[sti2].add(sti1)
-        except KeyError:
-            pass
+    '''Counterpart of GFA.addTransition'''
+    if sti1 not in gfa.delta:
+        gfa.delta[sti1] = {}
+    if sti2 not in gfa.delta[sti1]:
+        gfa.delta[sti1][sti2] = sym
+    else:
+        gfa.delta[sti1][sti2] = reex.CDisj(gfa.delta[sti1][sti2], sym, copy.copy(gfa.Sigma))
+    try:
+        gfa.predecessors[sti2].add(sti1)
+    except KeyError:
+        pass
