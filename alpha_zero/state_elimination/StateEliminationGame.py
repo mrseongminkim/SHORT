b824b0b6c7927c78adf101c4aa06ebdcc86ddd0d
Seongmin Kim <mr.seongminkim@gmail.com>
2023-03-05 19:14:21 +0900

    Update StateEliminationGame.py

--- alpha_zero/state_elimination/StateEliminationGame.py
+++ alpha_zero/state_elimination/StateEliminationGame.py
@@ -7,20 +7,21 @@ class StateEliminationGame(Game):
     def __init__(self, maxN = 18):
         self.maxN = maxN
 
-    def getInitBoard(self, gfs, n, k, d):
-        self.n = n
+    def getInitBoard(self, gfs = None, n = None, k = None, d = None):
+        if gfs == None:
+            n = np.random.randint(5, 11) #np.random.randint(3, maxN)
+            k = 5 #np.random.choice([2, 5, 10])
+            d = 0.2 #np.random.choice([0.2, 0.5])
+            gfs = generate(self.n, self.k, self.d, 'in-memory')
+        self.n = n + 2
         self.k = k
         self.d = d
         self.gfs = gfs.dup()
-        #self.n = np.random.randint(5, 11) #np.random.randint(3, maxN)
-        #self.k = 5 #np.random.choice([2, 5, 10])
-        #self.d = 0.2 #np.random.choice([0.2, 0.5])
-        #self.gfs = generate(self.n, self.k, self.d, 'in-memory')
         board = np.zeros((self.maxN + 2, self.maxN + 2), dtype = int)
         for source_state in self.gfs.delta:
             for target_state in self.gfs.delta[source_state]:
                 board[source_state][target_state] = self.gfs.delta[source_state][target_state].treeLength()
-        # 0 as initial, self.n + 1 as final
+        # 0 as initial, self.n - 1 as final
         return board
     
     def getBoardSize(self):
@@ -29,6 +30,7 @@ class StateEliminationGame(Game):
     def getActionSize(self):
         return self.maxN
 
+    #error-pron code
     def getNextState(self, board, player, action):
         new_board = np.copy(board)
         action += 1
@@ -49,16 +51,16 @@ class StateEliminationGame(Game):
 
     def getValidMoves(self, board, player):
         validMoves = [0 for i in range(self.maxN)]
-        for i in range(1, self.n + 1):
+        for i in range(1, self.n - 1):
             validMoves[i - 1] = int(np.any(board[i]))
         return validMoves
 
     def getGameEnded(self, board, player):
         #-1 as not finished, value for transition
-        for i in range(1, self.n + 1):
+        for i in range(1, self.n - 1):
             if np.any(board[i]):
                 return -1
-        return board[0][self.n + 1] / (3.33) ** (self.n)
+        return board[0][self.n - 1] / (3.33) ** (self.n)
 
     def getCanonicalForm(self, board, player):
         return board
