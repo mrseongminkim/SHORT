[fix] reward scaling & NN tuning

alpha_zero/state_elimination/StateEliminationGame.py
@@ -70,7 +70,7 @@ class StateEliminationGame(Game):
         
         if board[1:self.n - 1, 1:self.n - 1].sum() != 0:
             return -1
-        return (1 / board[0][self.n - 1]) * 2 - 1
+        return - np.log(board[0][self.n - 1])
 
     def getCanonicalForm(self, board, player):
         return board

alpha_zero/state_elimination/pytorch/NNet.py
@@ -12,9 +12,9 @@ from alpha_zero.state_elimination.pytorch.StateEliminationNNet import StateElimi
 
 args = dotdict({
     'lr': 0.001,
-    'dropout': 0.3,
+    'dropout': 0.2,
     'epochs': 10,
-    'batch_size': 64,
+    'batch_size': 32,
     'cuda': torch.cuda.is_available(),
     'num_channels': 512,
 })

alpha_zero/state_elimination/pytorch/StateEliminationNNet.py
@@ -23,12 +23,12 @@ class StateEliminationNNet(nn.Module):
         self.bn3 = nn.BatchNorm2d(args.num_channels)
         self.bn4 = nn.BatchNorm2d(args.num_channels)
         self.fc1 = nn.Linear(
-            args.num_channels*(self.board_x-4)*(self.board_y-4), 1024)
-        self.fc_bn1 = nn.BatchNorm1d(1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc_bn2 = nn.BatchNorm1d(512)
-        self.fc3 = nn.Linear(512, self.action_size)
-        self.fc4 = nn.Linear(512, 1)
+            args.num_channels*(self.board_x-4)*(self.board_y-4), 2048)
+        self.fc_bn1 = nn.BatchNorm1d(2048)
+        self.fc2 = nn.Linear(2048, 256)
+        self.fc_bn2 = nn.BatchNorm1d(256)
+        self.fc3 = nn.Linear(256, self.action_size)
+        self.fc4 = nn.Linear(256, 1)
 
     def forward(self, s):
         #                                                           s: batch_size x board_x x board_y
@@ -52,4 +52,4 @@ class StateEliminationNNet(nn.Module):
         pi = self.fc3(s)
         # batch_size x 1
         v = self.fc4(s)
-        return F.log_softmax(pi, dim=1), torch.tanh(v)
+        return F.log_softmax(pi, dim=1), v