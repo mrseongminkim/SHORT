fbfc106f5c2581acbcebe6b2463b2a5ba5fa8cc1
Seongmin Kim <mr.seongminkim@gmail.com>
2023-03-13 02:11:08 +0900

    Update reduction.py

--- utils/reduction.py
+++ utils/reduction.py
@@ -59,14 +59,72 @@ def minimize_regular_expression(regex: RegExp) -> tuple:
         print('further improvement?')
         return regex, [regex]
 
+def rpn(regex: RegExp) -> str:
+    if isinstance(regex, CAtom):
+        return regex.val
+    if isinstance(regex, CEpsilon):
+        return Epsilon
+    if isinstance(regex, CDisj):
+        return rpn(regex.arg1) + rpn(regex.arg2) + '+'
+    if isinstance(regex, CStar):
+        return rpn(regex.arg) + '*'
+    if isinstance(regex, CConcat):
+        return rpn(regex.arg1) + rpn(regex.arg2) + '.'
+
+def concat_list(waiting_list: list) -> RegExp:
+    if isinstance(waiting_list[0], RegExp):
+        result = waiting_list[0]
+    else:
+        result = CAtom(waiting_list[0])
+    for i in range(1, len(waiting_list)):
+        result = CConcat(result, waiting_list[i])
+    return result
+
+def rpn_to_str(rpn: str) -> RegExp:
+    waiting_list = []
+    stack = []
+    for c in rpn:
+        if c == '.':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            if arg1 != 'dummy':
+                waiting_list.append(arg1)
+            if arg2 != 'dummy':
+                waiting_list.append(arg2)
+            stack.append('dummy')
+        elif c == '+':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            #arg1 and arg2 can not be both dummy i hope
+            if arg1 == 'dummy':
+                arg1 = concat_list(waiting_list)
+            elif arg2 == 'dummy':
+                arg2 = concat_list(waiting_list)
+#########won't work as you intended.
+            pass
+    if waiting_list:
+        pass
+
+
+
 def main():
-
-    '(0 1) + (0 + 1)'
-    '((0 (1 2)) + ((0 1) 2))**'
-    '(0 (1 (2 3))) + ((0 1)(2 3))'
-
-    '(0 ((1 + 2) 3)) + ((0 (1 + 2)) 3)'
+    #result = '0 (1 2) + 3'
+    #result = '(0 1) + (0 + 1)'
+    #'((0 (1 2)) + ((0 1) 2))**'
+    #'(0 (1 (2 3))) + ((0 1)(2 3))'
+    result = '(0 (1 2))'
+    result = '(0 1) 2'
+    #result = '0 (1 + 2) 3'
+    #result = '(0 (1 + 2)) 3'
+    #result = '(0 ((1 + 2) 3)) + ((0 (1 + 2)) 3)'
+    result = '0 1 2 + 0 (1 2)'
     result = str2regexp(result)
+    print(result)
+    print(repr(result))
+    #rpn2regexp()
+    result = rpn(result)
+    print(result)
+    exit()
 
     #((0 (1 2)) + ((0 1) 2))**
     #result = CStar(CStar(CDisj(CConcat(CAtom(0), CConcat(CAtom(1), CAtom(2))), CConcat(CConcat(CAtom(0), CAtom(1)), CAtom(2)))))
