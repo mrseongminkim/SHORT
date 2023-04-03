d0336c267cc3f9daabdff0eaac3d6ac2e62426ff
Seongmin Kim <mr.seongminkim@gmail.com>
2023-04-03 14:30:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import random
 import csv
 import itertools
+from pickle import load, dump
 
 import numpy as np
 import coloredlogs
@@ -240,9 +241,50 @@ def quicky():
     for n in range(3 - 3, 8 - 3):
         print(n + 3, exp[n][1][0][1] / 100)
 
+def test_reduction() -> list:
+    alphabet_list = [5]
+    density_list = ['s']
+
+    data = [[[[0, 0, 0] for d in range(1)] for k in range(1)] for n in range(5)]
+    for n in range(3, 8):
+        for k in alphabet_list:
+            for d in density_list:
+                file_name = 'n' + str(n) + 'k' + str(5) + "s"
+                content = readFromFile('data/raw/' + file_name + '.txt')
+                for i in range(len(content)):
+                    print(n, k, d, i)
+                    nfa = content[i].dup()
+                    nfa.reorder({(content[i].States).index(x) : int(x) for x in content[i].States})
+
+                    temp = nfa.dup()
+                    data[n - 3][0][0][0] += len(temp.rEquivNFA().States)
+
+                    temp = nfa.dup()
+                    data[n - 3][0][0][1] += len(temp.lEquivNFA().States)
+
+                    temp = nfa.dup()
+                    data[n - 3][0][0][2] += len(temp.lrEquivNFA().States)
+                    for q in range(3):
+                        print(data[n - 3][0][0][q], end = "\t")
+
+    with open('data/' + "reduction" + '.pkl', 'wb') as fp:
+        dump(data, fp)
+
+def check_result():
+    data = list()
+    with open('data/' + "reduction" + '.pkl', 'rb') as fp:
+        data = load(fp)
+    for n in range(3, 8):
+        print(data[n - 3][0][0])
+
 
 def main():
     train_alpha_zero()
 
 
-main()
+regex = "0 (0 + 1)"
+hi = str2regexp(regex)
+hi2 = deepcopy(hi)
+print("", regex, "\n", hi)
+test = CToken(hi)
+test2 = CToken(hi2)
