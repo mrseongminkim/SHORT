187787493a1806a9cd24657aeb436a9d927969ca
Seongmin Kim <mr.seongminkim@gmail.com>
2023-04-08 22:43:34 +0900

    [update] complete dfa generator

--- utils/random_dfa_generator.py
+++ utils/random_dfa_generator.py
@@ -1,6 +1,9 @@
 import time
 
 from FAdo.rndfap import *
+from FAdo.fa import *
+
+from utils.fadomata import *
 
 seed = hash(time.perf_counter())
 
@@ -10,6 +13,22 @@ generator = ICDFArgen(n, k, seed=seed)
 
 count = 0
 while (count < 100):
-    dfa = generator.next()
+    dfa: DFA = generator.next()
     if dfa.Final:
         count += 1
+    else:
+        continue
+    nfa = dfa.toNFA()
+    make_nfa_complete(nfa)
+    order = {}
+    for i in range(len(nfa.States)):
+        if i == len(nfa.States) - 2:
+            order[i] = 0
+        elif i == len(nfa.States) - 1:
+            order[i] = i
+        else:
+            order[i] = i + 1
+    nfa.reorder(order)
+    nfa.renameStates()
+    gfa = convert_nfa_to_gfa(nfa)
+    exit()
