75df12643c20c46387c4bc796ea94c92eb4daa57
Seongmin Kim <mr.seongminkim@gmail.com>
2023-04-08 22:14:22 +0900

    [fix] restore learning rate

--- alpha_zero/state_elimination/pytorch/NNet.py
+++ alpha_zero/state_elimination/pytorch/NNet.py
@@ -8,8 +8,7 @@ from alpha_zero.utils import AverageMeter, dotdict
 from alpha_zero.state_elimination.pytorch.StateEliminationNNet import StateEliminationNNet as sennet
 
 args = dotdict({
-    #0.001
-    'lr': 0.01,
+    'lr': 0.001,
     'dropout': 0.0,
     'epochs': 20,
     'batch_size': 32,
