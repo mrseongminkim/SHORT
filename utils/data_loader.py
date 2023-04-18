ffa10f6704ff028a2d2047b2d63830a3c4f9d0de
Seongmin Kim <mr.seongminkim@gmail.com>
2023-04-18 20:38:52 +0900

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -21,30 +21,6 @@ def load_nfa():
                 dump(content, fp)
             data[n] = content
     return data
-    '''
-    alphabet_list = [2, 5, 10]
-    density_list = ['s', 'd']
-    data = [[[[] for d in range(2)] for k in range(3)] for n in range(8)]
-    for n in range(3, 11):
-        for k in alphabet_list:
-            for d in density_list:
-                a = n - 3
-                b = alphabet_list.index(k)
-                c = density_list.index(d)
-                file_name = 'n' + str(n) + 'k' + str(k) + d
-                if isfile('data/random_nfa/pkl/' + file_name + '.pkl'):
-                    with open('data/random_nfa/pkl/' + file_name + '.pkl', 'rb') as fp:
-                        data[a][b][c] = load(fp)
-                else:
-                    content = readFromFile('data/raw/' + file_name + '.txt')
-                    for i in range(len(content)):
-                        content[i] = convert_nfa_to_gfa(content[i])
-                        content[i].reorder({(content[i].States).index(x) : int(x) for x in content[i].States})
-                    with open('data/random_nfa/pkl/' + file_name + '.pkl', 'wb') as fp:
-                        dump(content, fp)
-                    data[a][b][c] = content
-    return data
-    '''
 
 
 def load_position():
@@ -71,6 +47,7 @@ def load_fig10():
     #nfa.display()
     gfa = convert_nfa_to_gfa(nfa)
     return gfa
+
 
 def load_data(type):
     if type == 'nfa':
