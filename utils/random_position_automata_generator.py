c58db8abac1e7f37a67993fbd80ec869e7c3b329
Seongmin Kim <mr.seongminkim@gmail.com>
2023-04-22 01:57:10 +0900

    Update random_position_automata_generator.py

--- utils/random_position_automata_generator.py
+++ utils/random_position_automata_generator.py
@@ -1,4 +1,5 @@
 from pickle import load, dump
+import csv
 
 from FAdo.cfg import *
 from FAdo.reex import *
@@ -6,9 +7,10 @@ from FAdo.reex import *
 from utils.fadomata import *
 from utils.heuristics import *
 
-data = [[None, None] for i in range(100)]
+data = []
+original_length = 0
 
-reverse = True
+reverse = False
 states = 20
 length = 30
 Sigma = ['0', '1', '2', '3', '4']
@@ -20,15 +22,21 @@ while count < 100:
     position_automata: NFA = regular_expression_to_position_automata(regular_expression)
     if len(position_automata.States) != states:
         continue
+    original_length += regular_expression.treeLength()
     make_nfa_complete(position_automata)
     if reverse:
         reorder_reverse(position_automata, states)
     else:
         reorder(position_automata, states)
     gfa = convert_nfa_to_gfa(position_automata)
-    data[count][0] = gfa
-    data[count][1] = regular_expression.treeLength()
+    data.append(gfa)
     count += 1
 
-with open('data/pos_s20_l30r.pkl', 'wb') as fp:
+data = [data]
+
+with open('data/pos_s20_l30_r.pkl', 'wb') as fp:
     dump(data, fp)
+
+with open('./result/original_length_r.csv', 'w', newline='') as fp:
+    writer = csv.writer(fp)
+    writer.writerow([original_length / 100])
