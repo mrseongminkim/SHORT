e8048e1650d9cfc2f3f789e473bb327de8084aee
Seongmin Kim <mr.seongminkim@gmail.com>
2023-06-07 19:52:41 +0900

    [update] global constants for short

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+#Test parameters
+SAMPLE_SIZE = 1_000
+
 #NFA parameters
 MIN_N = 3
 MAX_N = 10
 N_RANGE = MAX_N - MIN_N + 1
+STATE = [n for n in range(MIN_N, MAX_N + 1)]
+ALPHABET = [5]
+DENSITY = [0.2]
