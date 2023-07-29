f96ca95e3f6c258a8707bb0ffac06fc4703459e2
Seongmin Kim <mr.seongminkim@gmail.com>
2023-07-30 05:09:53 +0900

    [update] save model

--- lstm_pretrain_network.py
+++ lstm_pretrain_network.py
@@ -9,26 +9,39 @@ from torch.utils.data import Dataset, Subset, DataLoader
 
 from config import *
 
+class EmbeddingWithLSTM(nn.Module):
+    def __init__(self):
+        super(EmbeddingWithLSTM, self).__init__()
+        self.vocab_size = VOCAB_SIZE
+        self.regex_embedding_dim = REGEX_EMBEDDING_DIMENSION
+        self.lstm_dim = LSTM_DIMENSION
+        self.embed = nn.Embedding(self.vocab_size, self.regex_embedding_dim, padding_idx=0)
+        self.lstm = nn.LSTM(self.regex_embedding_dim, self.lstm_dim, batch_first=True, bidirectional=True)
+    
+    def forward(self, regex):
+        regex = self.embed(regex)
+        regex, _ = self.lstm(regex)
+        return regex
+
 class RegexNNet(nn.Module):
     def __init__(self):
         super(RegexNNet, self).__init__()
         self.vocab_size = VOCAB_SIZE
         self.regex_embedding_dim = REGEX_EMBEDDING_DIMENSION
         self.lstm_dim = LSTM_DIMENSION
-        self.embedding = nn.Embedding(self.vocab_size, self.regex_embedding_dim, padding_idx=0)
-        self.lstm1 = nn.LSTM(self.regex_embedding_dim, self.lstm_dim, num_layers=1, batch_first=True, bidirectional=True)
-        self.lstm2 = nn.LSTM(self.regex_embedding_dim, self.lstm_dim, num_layers=1, batch_first=True, bidirectional=True)
+        self.embedding_lstm = EmbeddingWithLSTM()
+        self.embedding = self.embedding_lstm.embed
+        self.lstm = nn.LSTM(self.regex_embedding_dim, self.lstm_dim, num_layers=1, batch_first=True, bidirectional=True)
         self.fc1 = nn.Linear(self.lstm_dim * 2 * 2, 60)
         self.fc2 = nn.Linear(60, 20)
         self.fc3 = nn.Linear(20, 2)
 
     def forward(self, regex1, regex2):
-        regex1 = self.embedding(regex1)
-        regex1, _ = self.lstm1(regex1)
+        regex1 = self.embedding_lstm(regex1)
         regex1 = regex1.mean(1)
 
         regex2 = self.embedding(regex2)
-        regex2, _ = self.lstm2(regex2)
+        regex2, _ = self.lstm(regex2)
         regex2 = regex2.mean(1)
 
         concat = torch.cat((regex1, regex2), 1)
@@ -56,25 +69,24 @@ class RegexDataset(Dataset):
             label = torch.LongTensor([0, 1])
         return regex1, regex2, label
 
-epochs = 200
+epochs = 5000
 batch_size = 256
 lr = 0.1
 
-train_size = 400_000
-valid_size = 10_000
-test_size = 10_000
+train_size = 500_000
+valid_size = 20_000
 regex_data = RegexDataset("./annotations_file.csv")
 
 train_data = Subset(regex_data, torch.arange(train_size))
 valid_data = Subset(regex_data, torch.arange(train_size, train_size + valid_size))
-test_data = Subset(regex_data, torch.arange(train_size + valid_size, train_size + valid_size + test_size))
 train = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 valid = DataLoader(valid_data, batch_size=batch_size, shuffle=False)
-test = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
 model = RegexNNet().cuda()
 criterion = nn.NLLLoss()
 optimizer = optim.SGD(model.parameters(), lr=lr)
+
+minimum_loss = float("inf")
 
 print(f"Train on {train_size}, validate on {valid_size} samples.")
 for epoch in range(epochs):
@@ -106,17 +118,6 @@ for epoch in range(epochs):
             total_loss += loss.item()
             count += 1
         print(f"Valid - Epoch {epoch + 1}, loss: {total_loss / count}")
-
-with torch.no_grad():
-    model.eval()
-    total_loss = 0
-    count = 0
-    for regex1, regex2, label in test:
-        regex1 = regex1.cuda()
-        regex2 = regex2.cuda()
-        label = label.cuda()
-        result = model(regex1, regex2)
-        loss = criterion(result, label[:, 1])
-        total_loss += loss.item()
-        count += 1
-    print(f"Test - Epoch {epoch + 1}, loss: {total_loss / count}")
+        if total_loss < minimum_loss:
+            minimum_loss = total_loss
+            torch.save(model.embedding_lstm.state_dict(), f"./{epoch}embed_lstm.pth")
